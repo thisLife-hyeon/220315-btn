f8a14cea31d6dbcb4e02574dbd72b3f2f91e57e8
thisLife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-03-15 20:58:32 +0900

    i don't know

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
+const btns = document.querySelectorAll("btn__bg");
 const btn1 = document.querySelector(".btn__bg--con1");
 const btn2 = document.querySelector(".btn__bg--con2");
 const btn3 = document.querySelector(".btn__bg--con3");
-const btn4 = document.querySelector(".btn__bg--con4");
-
 const colors = ["bgcolor--orange", "bgcolor--green", "bgcolor--purple"];
 
-// btn1.addEventListener("click", function () {
-//   document.body.classList.toggle(colors[0]);
-//   document.body.classList.remove(colors[(0, 1, 2, 3)]);
-// });
+function btnOnClick() 
 
-// btn2.addEventListener("click", function () {
-//   document.body.classList.toggle(colors[1]);
-//   document.body.classList.remove(colors[(0, 1, 2, 3)]);
-// });
-
-// btn3.addEventListener("click", function () {
-//   document.body.classList.toggle(colors[2]);
-//   document.body.classList.remove(colors[(0, 1, 2, 3)]);
-// });
-
-function removeColor() {
-  document.body.classList.remove(colors[(0, 1, 2, 3)]);
+  btns.forEach((btn) => {
+    document.body.classList.remove(colors[(0, 1, 2, 3)]);
 }
+  );
 
-function onBtn1() {
-  removeColor(document.body.classList.toggle(colors[0]));
-}
-function onBtn2() {
-  removeColor(document.body.classList.toggle(colors[1]));
-}
-function onBtn3() {
-  removeColor(document.body.classList.toggle(colors[2]));
-}
 
-btn1.addEventListener("click", onBtn1);
-btn2.addEventListener("click", onBtn2);
-btn3.addEventListener("click", onBtn3);
+btn1.addEventListener("click", function () {
+    btn.removeColor();
+    document.body.classList.add(colors[0]);
+  });
+  btn2.addEventListener("click", function () {
+    btn.removeColor();
+    document.body.classList.add(colors[1]);
+  });
+  btn3.addEventListener("click", function () {
+    btn.removeColor();
+    document.body.classList.add(colors[2]);
+  });
+
+window.addEventListener("click", btnOnClick);
+btnOnClick();
