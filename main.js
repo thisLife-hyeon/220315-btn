60942e1a83f8a841ca5175b388085ce928ca2b71
thisLife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-03-16 10:16:18 +0900

    forEach

--- main.js
+++ main.js
@@ -4,26 +4,26 @@ const btn2 = document.querySelector(".btn__bg--con2");
 const btn3 = document.querySelector(".btn__bg--con3");
 const colors = ["bgcolor--orange", "bgcolor--green", "bgcolor--purple"];
 
-function btnOnClick() 
-
+function resetColor() {
+  document.body.classList.remove(colors[(0, 1, 2, 3)]);
   btns.forEach((btn) => {
-    document.body.classList.remove(colors[(0, 1, 2, 3)]);
+    btn.addEventListener("click, resetColor");
+  });
 }
-  );
 
+function onBtn1() {
+  resetColor();
+  document.body.classList.add(colors[0]);
+}
+function onBtn2() {
+  resetColor();
+  document.body.classList.add(colors[1]);
+}
+function onBtn3() {
+  resetColor();
+  document.body.classList.add(colors[2]);
+}
 
-btn1.addEventListener("click", function () {
-    btn.removeColor();
-    document.body.classList.add(colors[0]);
-  });
-  btn2.addEventListener("click", function () {
-    btn.removeColor();
-    document.body.classList.add(colors[1]);
-  });
-  btn3.addEventListener("click", function () {
-    btn.removeColor();
-    document.body.classList.add(colors[2]);
-  });
-
-window.addEventListener("click", btnOnClick);
-btnOnClick();
+btn1.addEventListener("click", onBtn1);
+btn2.addEventListener("click", onBtn2);
+btn3.addEventListener("click", onBtn3);
