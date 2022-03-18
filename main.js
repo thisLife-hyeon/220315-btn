3ec0347dbdf80d06ad6a5b231c80f9646a4993d4
thisLife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-03-18 18:35:12 +0900

    add 4th button

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const btns = document.querySelectorAll("btn__bg");
 const btn1 = document.querySelector(".btn__bg--con1");
 const btn2 = document.querySelector(".btn__bg--con2");
 const btn3 = document.querySelector(".btn__bg--con3");
-const colors = ["bgcolor--orange", "bgcolor--green", "bgcolor--purple"];
+const btn4 = document.querySelector(".btn__bg--con4");
+const colors = ["bgcolor--orange", "bgcolor--green", "bgcolor--purple", "bgcolor--plant"];
 
 function resetColor() {
-  document.body.classList.remove(colors[0]);
-  document.body.classList.remove(colors[1]);
-  document.body.classList.remove(colors[2]);
+  document.body.classList.remove(colors[0], colors[1], colors[2], colors[3]);
   btns.forEach((btn) => {
     btn.addEventListener("click", resetColor);
   });
@@ -25,7 +24,12 @@ function onBtn3() {
   resetColor();
   document.body.classList.add(colors[2]);
 }
+function onBtn4() {
+  resetColor();
+  document.body.classList.add(colors[3]);
+}
 
 btn1.addEventListener("click", onBtn1);
 btn2.addEventListener("click", onBtn2);
 btn3.addEventListener("click", onBtn3);
+btn4.addEventListener("click", onBtn4);
